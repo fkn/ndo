aa1d162941276612be9f110099b4cbde3f2fa62f
maxkhog <maxkhog@gmail.com>
2018-09-26 22:03:35 +0800

    add MarksList

--- src/routes/course-marks/AnswerList.js
+++ src/routes/course-marks/AnswerList.js
@@ -2,35 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Table } from 'react-bootstrap';
 
-const AnswerList = ({ answers, stydyEntity }) => (
+const AnswerList = ({ studyEntity }) => (
   <Table>
     <thead>
       <tr>
-        <th scope="col">{stydyEntity.title}</th>
+        <th scope="col">{studyEntity.title}</th>
         <th scope="col">Mark</th>
       </tr>
-      {answers.map(ans => (
+      {studyEntity.answers[0].marks.map(mark => (
         <tr>
-          <td>{ans.title}</td>
-          <td>{ans.mark}</td>
+          <td>{mark.createdAt.substr(0, 10) || 'No'}</td>
+          <td>{mark.mark.toFixed(2) || 'No'}</td>
         </tr>
       ))}
     </thead>
   </Table>
 );
 AnswerList.propTypes = {
-  answers: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string,
-      title: PropTypes.string,
-    }),
-  ).isRequired,
-  stydyEntity: PropTypes.shape({
-    title: PropTypes.string.isRequired,
+  studyEntity: PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string,
     answers: PropTypes.arrayOf(
       PropTypes.shape({
         id: PropTypes.string,
-        title: PropTypes.string,
+        marks: PropTypes.arrayOf(
+          PropTypes.shape({
+            createdAt: PropTypes.string,
+            id: PropTypes.string,
+            mark: PropTypes.float,
+          }),
+        ),
       }),
     ),
   }).isRequired,
