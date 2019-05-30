3bf47ffabba45bae39fd404dc0fbfc303577ca6d
ilyaabramovich <ilyaabramovich1998@gmail.com>
2019-05-30 13:12:25 +0800

    add CJTest graphql queries

--- src/data/types/CjProblemType.js
+++ src/data/types/CjProblemType.js
@@ -1,14 +1,20 @@
 import {
+  GraphQLList as List,
   GraphQLNonNull as NonNull,
   GraphQLObjectType as ObjectType,
   GraphQLString as StringType,
 } from 'graphql';
+import CjTestType from './CjTestType';
 
 const CjProblemType = new ObjectType({
   name: 'CjProblemType',
   fields: () => ({
     id: { type: new NonNull(StringType) },
     title: { type: new NonNull(StringType) },
+    tests: {
+      type: new List(CjTestType),
+      resolve: problem => problem.getTests(),
+    },
   }),
 });
 
