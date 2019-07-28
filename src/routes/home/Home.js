b8061feffb4c95a1c60d96a2cdae1f22ee2e969f
Ilya <ilyaabramovich1998@gmail.com>
2019-07-28 17:01:42 +0800

    remove useless ChatWS component

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
-import ChatWS from '../../components/ChatWS';
+import ChatRoom from '../../components/ChatRoom';
 
-function Home() {
-  return (
-    <div className={s.root}>
-      <div className={s.container}>
-        <h1>NDO</h1>
-        <ChatWS />
-      </div>
+const Home = ({ user: { email = '' } }) => (
+  <div className={s.root}>
+    <div className={s.container}>
+      <h1>NDO</h1>
+      <ChatRoom name={email} />
     </div>
-  );
-}
+  </div>
+);
+
+Home.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default withStyles(s)(Home);
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Home from './Home';
 import Layout from '../../components/Layout';
 
-function action() {
+function action({ store }) {
+  const { user } = store.getState();
   return {
     title: 'NDO',
     chunks: ['home'],
     component: (
       <Layout>
-        <Home />
+        <Home user={user} />
       </Layout>
     ),
   };
