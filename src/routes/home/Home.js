8d55b86f9891165d4fbc08709bd50fb04167c1cd
Ilya <ilyaabramovich1998@gmail.com>
2019-07-29 15:19:16 +0800

    fix bug

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -4,11 +4,11 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 import ChatRoom from '../../components/ChatRoom';
 
-const Home = ({ user: { email = '' } }) => (
+const Home = ({ user }) => (
   <div className={s.root}>
     <div className={s.container}>
       <h1>NDO</h1>
-      <ChatRoom name={email} />
+      <ChatRoom name={user ? user.email : ''} />
     </div>
   </div>
 );
